733a1f616429420419c30127cb60e1367dc4d6ee
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-20 11:07:36 -0500

    Agregar archivos de proyecto.

---
+++ Capitulo 4 ejercicio 9/Program.cs
@@ -0,0 +1,46 @@
+﻿class ejer
+{
+
+
+    static public void Main()
+    {
+
+
+
+
+        int n3;
+        int n4;
+        String SoR;
+        int result;
+
+        Console.WriteLine("escriba el primero numero: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("escriba el segundo numero: ");
+        n4 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("para sumar digite S y para restar digite R");
+        SoR = Console.ReadLine();
+
+        switch (SoR)
+        {
+
+            case "S":
+
+                result = n3 + n4;
+                Console.WriteLine($"el resultado es:{result}");
+                break;
+
+            case "R":
+
+                result = n3 - n4;
+                Console.WriteLine($"el resultado es: {result}");
+                break;
+
+            default:
+                Console.WriteLine("no solecciono suma o resta");
+                break;
+}
+
+    }
+}
